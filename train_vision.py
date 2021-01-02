6fe714c788a0ec5bfd32d88595b9033d2ec6cc5c
Yonatan Bisk <ybisk@yonatanbisk.com>
2021-01-02 17:52:40 -0500

    let's try and be less lazzzzzy

--- train_vision.py
+++ train_vision.py
@@ -7,42 +7,71 @@ from tqdm import tqdm
 from PIL import Image
 import numpy as np
 import torchvision
+import torchvision.models as models
+from torchvision import transforms as T
 
 
-fnames = ["vision_training/imgs/{}.png".format(line.strip().split()[0]) for line in open("vision_training/labels.txt")]
-labels = [np.array([float(v) for v in line.strip().split()[1:]]).astype(np.float32) for line in open("vision_training/labels.txt")]
-images = [np.array(Image.open(fname))/255 for fname in fnames]
-images = [np.transpose(v, (2,0,1)).astype(np.float32) for v in images]
+
+# [-40, 40], [240, 300], [-40, 0]
+ranges = np.array([80, 60, 40]).astype(np.float32)
+mins = np.array([-40, 240, -40]).astype(np.float32)
+
+def norm_lbl(label):
+  label = np.array(label)
+  label -= mins     # Shift everything to start at 0
+  label /= ranges/2 # Normalize to 2
+  label -= 1
+  return label.astype(np.float32)
+
+norm = T.Normalize(mean=[0.485, 0.456, 0.406],
+                        std=[0.229, 0.224, 0.225])
+norm_img = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor(), norm])
+
+fnames = ["vision_training/imgs_orig/{}.png".format(line.strip().split()[0]) for line in open("vision_training/labels.txt")]
+labels = [norm_lbl([float(v) for v in line.strip().split()[1:]]) for line in open("vision_training/labels.txt")]
+images = [norm_img(Image.open(fname)) for fname in fnames]
 trainloader = torch.utils.data.DataLoader(list(zip(images, labels)), batch_size=8, shuffle=True, num_workers=2)
 print("Loaded Data")
+
 
 class Net(nn.Module):
   def __init__(self):
     super(Net, self).__init__()
-    # 160 x 120
-    self.pool = nn.MaxPool2d(2, 2) 
-    self.conv1 = nn.Conv2d(3, 6, 5)           # 160/120 - 156 --> 78
-    self.conv2 = nn.Conv2d(6, 12, 5)          #  78/ 58 -  74 --> 37
-    self.conv3 = nn.Conv2d(12, 24, 5)         #  37/ 27 -  33 --> 16
-    self.conv4 = nn.Conv2d(24, 48, 5)         #  16/ 11 -  12 --> 6/3
-    self.fc1 = nn.Linear(3 * 6 * 48, 3 ** 4)
-    self.fc2 = nn.Linear(3 ** 4, 3)
+    #self.vpl = models.resnet18(pretrained=True)
+    self.vpl = models.squeezenet1_1(pretrained=True)
+    self.choice = "squeeze"
+
+    self.fc1 = nn.Linear(512, 128)
+    self.fc2 = nn.Linear(128, 32)
+    self.fc3 = nn.Linear(32, 3)
 
   def forward(self, x):
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
-    x = self.pool(F.relu(self.conv3(x)))
-    x = self.pool(F.relu(self.conv4(x)))
-    x = x.view(-1, 3 * 6 * 48)
+    if self.choice == "squeeze":
+      with torch.no_grad():
+        # https://pytorch.org/docs/stable/_modules/torchvision/models/squeezenet.html#squeezenet1_1
+        x = self.vpl.features(x)
+        x = nn.functional.adaptive_avg_pool2d(x, 1).reshape(x.shape[0], -1)
+    else: 
+      with torch.no_grad():
+        # https://pytorch.org/docs/stable/_modules/torchvision/models/resnet.html#resnet18
+        x = self.vpl.maxpool(self.vpl.relu(self.vpl.bn1(self.vpl.conv1(x))))
+        x = self.vpl.layer1(x)
+        x = self.vpl.layer2(x)
+        x = self.vpl.layer3(x)
+        x = self.vpl.layer4(x)
+        x = self.vpl.avgpool(x)
+        x = torch.flatten(x, 1)
+
     x = F.relu(self.fc1(x))
-    x = self.fc2(x)
+    x = F.relu(self.fc2(x))
+    x = self.fc3(x)
     return x
 
 net = Net()
-crit = nn.MSELoss()
+crit = nn.L1Loss() # MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=5e-5)
 
-for epoch in range(10):
+for epoch in range(50):
   running_loss = 0.0
   for i, data in tqdm(enumerate(trainloader, 0)):
     inputs, labels = data
@@ -56,3 +85,5 @@ for epoch in range(10):
     if i % 25 == 24:
       print('[{}, {:5d}] loss: {:.3f}'.format(epoch +1, i + 1, running_loss / 25)) 
       running_loss = 0.0
+      for l, o in zip(labels, outputs.detach().numpy()):
+        print("{:5.2f} {:5.2f} {:5.2f} -- {:5.2f} {:5.2f} {:5.2f}".format(l[0], l[1], l[2], o[0], o[1], o[2]))
