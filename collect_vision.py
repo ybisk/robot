03288b7ba214a04a21b3ea844c41351f48183c80
Yonatan Bisk <ybisk@yonatanbisk.com>
2021-01-01 20:40:27 -0500

    saving as individual files to avoid RAM overhead issues

--- collect_vision.py
+++ collect_vision.py
@@ -1,7 +1,5 @@
-import cv2
-import h5py
+import cv2, os
 from tqdm import tqdm
-from matplotlib import pyplot as plt
 from robot import Robot
 
 
@@ -11,32 +9,29 @@ cam = cv2.VideoCapture(0)
 
 rbt = Robot()
 
+os.makedirs("vision_training")
+os.makedirs("vision_training/imgs")
 
-data = {"imgs":[], "locs":[]}
-X = [-80, 80]
+labels = open("vision_training/labels.txt",'wt')
+X = [-40, 40]
 Y = [240, 300]
 Z = [-40, 0]
 
 inc = 10
 
-for x in tqdm(range(X[0], X[1]+inc, inc)):
-    for y in range(Y[0], Y[1]+inc, inc):
+datum = 0
+for x in tqdm(range(X[0], X[1]+inc, inc), ncols=50):
+    for y in tqdm(range(Y[0], Y[1]+inc, inc), ncols=50):
         for z in range(Z[0], Z[1]+inc, inc):
             rbt.move(x,y,z)
             val, img = cam.read()
             if val:
-                data["imgs"].append(img)
-                data["locs"].append([x,y,z])
+                cv2.imwrite('vision_training/imgs/{}.png'.format(datum),img)
+                labels.write("{} {:4.2f} {:4.2f} {:4.2f}\n".format(datum, x,y,z))
+                datum += 1
 
-
-print("Collected: ", len(data["imgs"]))
-
-
-f = h5py.File('training.hdf5', 'w')
-f.create_dataset("imgs", data=data["imgs"])
-f.create_dataset("locs", data=data["locs"])
-f.close()
-
+labels.close()
+print("Collected: {}".format(datum))
 
 #img = data["imgs"][100][:,:,::-1]
 #plt.imshow(img)
